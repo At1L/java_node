fd620f3926bef1ffe3e48d9799235ad20db5fd16
Kozlov Arseny <jetpilotat1il@gmail.com>
2023-07-08 13:28:52 +0300

    add create, insert and find functions

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    int id;
+    String name;
+    List<Node> children;
+    public Node(String a) {
+        this.id = 0;
+        this.name = a;
+        this.children = new ArrayList<Node>();
+    }
+    public int getChildrenSize(){
+        return this.children.size();
+    }
+    public void insert(String name){
+        Node buff = new Node(name);
+        this.children.add(buff);
+    }
+
+    public Node find(String name){
+        for(int i = 0; i < this.children.size(); i++){
+            if(children.get(i).name.equals(name)){
+                return children.get(i);
+            }
+        }
+        for(int i = 0; i < this.children.size(); i++){
+            this.children.get(i).find(name);
+        }
+
+        return null;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ src/test/java/NodeTest.java
@@ -0,0 +1,29 @@
+import org.example.Node;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NodeTest {
+    @Test
+    void createNodeTest(){
+        Node node = new Node("a");
+        assertNotNull(node, "Node not created");
+        assertEquals("a", node.getName());
+    }
+
+    @Test
+    void insertTest(){
+        Node node = new Node("a");
+        node.insert("b");
+        assertEquals(1, node.getChildrenSize());
+    }
+
+    @Test
+    void findTest(){
+        Node node = new Node("a");
+        node.insert("b");
+        assertEquals("b", node.find("b").getName());
+        assertNull(node.find("c").getName());
+    }
+    
+}
