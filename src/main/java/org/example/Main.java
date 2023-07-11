093db9a468fe8ea2df7e9b51bb90ccf61c087898
Misha <mishaosipv@gmail.com>
2023-07-11 13:06:11 +0300

    clear main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Node node = new Node("a");
-        node.insert("b");
-        node.insert("c");
-        node.find("b").insert("d");
-        node.levelorderTraversal(node, 0);
+
     }
 }
