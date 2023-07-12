4a85d8a60675aed0db2b20521a1491afa38df48c
Kozlov Arseny <jetpilotat1il@gmail.com>
2023-07-12 11:20:51 +0300

    add tests

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -55,4 +55,13 @@ public class NodeTest {
         assertEquals("b", node.getName());
     }
 
+    @Test
+    void findSC(){
+        Node node = new Node("a");
+        node.insert("b");
+        node.insert("c");
+        node.find("b").insert("d");
+        assertEquals("d", node.find("d").getName());
+    }
+
 }
